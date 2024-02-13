dce6c61f8be245fcbed03a52a4f036ceec9c7ea8
chiefpat450119 <91508703+chiefpat450119@users.noreply.github.com>
2024-02-13 14:40:32 -0800

    Implemented adding of goals

--- src/main/model/Goal.java
+++ src/main/model/Goal.java
@@ -2,31 +2,45 @@ package model;
 
 import java.util.Date;
 
-// A log entry representing a strength target
-public class Goal extends LogEntry {
+// A log entry representing a strength target with a list of exercises
+public class Goal extends Record {
     private Date targetDate;
     private int targetWeight;
     private String description;
+    private boolean completed;
 
     // REQUIRES: name is not empty
-    // EFFECTS: Instantiates a Goal with given date and name
+    // EFFECTS: Instantiates a Goal with given date and name, not completed
     public Goal(Date date, String name, String description) {
         super(date, name);
+        this.name = name + " goal set on " + date.toString();
         this.description = description;
+        this.completed = false;
     }
 
-    // REQUIRES: targetWeight >= 0
+    // REQUIRES: targetWeight >= 0, exerciseName not empty or null
     // MODIFIES: this
-    // EFFECTS: Modifies the goal by setting the date to achieve and the target weight.
-    public void setTarget(Date targetDate, int targetWeight) {
+    // EFFECTS: Modifies the goal by setting the date to achieve and the target weight on exercise
+    // with given name.
+    public void addTarget(Date targetDate, int targetWeight, String exerciseName) {
         this.targetDate = targetDate;
-        this.targetWeight = targetWeight;
+        addExercise(new Exercise(exerciseName, targetWeight, 1, 10));
     }
 
     // EFFECTS: returns a string summary of the goal
     @Override
     public String summary() {
         return null; // TODO: Implement
+    }
+
+    // MODIFIES: this
+    // EFFECTS: sets completed to true
+    public void setCompleted() {
+        completed = true;
+    }
+
+    public boolean isCompleted() {
+        return completed;
     }
 
     public Date getTargetDate() {
--- src/main/ui/TrainingLogger.java
+++ src/main/ui/TrainingLogger.java
@@ -2,11 +2,85 @@ package ui;
 
 import model.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
 
 // A strength training log, with a list of log entries of various types.
 // Logs can be accessed or reports generated based on user input.
 public class TrainingLogger {
     private ArrayList<LogEntry> logs;
+    private Scanner input;
 
+    // EFFECTS: Runs the logger application
+    public TrainingLogger() {
+        runLogger();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: processes user input
+    private void runLogger() {
+        String command = null;
+
+        init();
+
+        System.out.println("Welcome to IronInsight, your personal strength training tracker.");
+
+        // Main app loop
+        while (true) {
+            break;
+        }
+
+        System.out.print("Bye! Thanks for using IronInsight");
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Initializes empty training log list and input
+    private void init() {
+        logs = new ArrayList<>();
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+    }
+
+    // EFFECTS: displays main menu of options to user
+    private void mainMenu() {
+        System.out.println("\nSelect from:");
+        System.out.println("\tg -> Create a goal");
+        System.out.println("\ta -> Add a training log entry");
+        System.out.println("\tv -> View your progress");
+        System.out.println("\tq -> quit");
+    }
+
+    private void goalMenu() {
+        // stub
+    }
+
+    private void logsMenu() {
+        // stub
+    }
+
+    private void progressMenu() {
+        // stub
+    }
+
+    private void addGoal() throws ParseException {
+        System.out.println("Enter a name for your goal");
+        String goalName = input.next();
+        System.out.println("Enter your target completion date in the format YYYY-MM-DD");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        String dateString = input.next();
+        Date goalDate = formatter.parse(dateString);
+        System.out.println("Enter your exercise name");
+        String exerciseName = input.next();
+        System.out.println("Enter your target weight");
+        int weight = input.nextInt();
+        System.out.println("Enter a brief description for your goal");
+        String description = input.next();
+        Goal goal = new Goal(new Date(), goalName, description);
+        goal.addTarget(goalDate, weight, exerciseName);
+        logs.add(goal);
+    }
 }
